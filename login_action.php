0aa80d18028238cc34eef8dfd864b85c7063d48f
Ch10316 <andrewlime04@naver.com>
2021-01-11 13:30:03 +0900

    11

--- login_action.php
+++ login_action.php
@@ -2,7 +2,9 @@
     session_start();
 
     $connect = mysqli_connect('localhost', 'root', '', 'board') or die ("Fail");
-    
+    $query = "select * from where id = ";
+    $result = $connect->query($query);
+
     //Id와 Pw 입력받기
     $id = $_GET['id'];
     $pw = $_GET['pw'];
@@ -15,9 +17,30 @@
         if($row['pw']==$pw){
             $_SESSION['userid'] = $id;
             if(isset($_SESSION['userid'])){
-            
-            ?>
+?>
+            <script>
+                alert("로그인이 되었습니다.");
+                location.replace("./index.php");
+            </script>
+<?php
             }
+            else{
+                echo "session fail";
+            }
+        }    
+        else{
+            ?><script>
+                alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+                history.back();
+            </script>
+            <?php
         }
-    }
+        else{
+            ?>
+            <script>
+                alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+                histroy.back();
+            </script>
+<?php
+        }
 ?>
