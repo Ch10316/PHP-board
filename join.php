d943cf48dfde41603942e96f6670eb140a066409
Ch10316 <andrewlime04@naver.com>
2021-01-11 13:58:20 +0900

    join

---
+++ join.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <div align="center">
+        <p>회원가입</p>
+        <form method='get' action="join_action.php">
+            <p>ID: <input name="id" type="text"></p>
+            <p>PW: <input name="pw" type="password"></p>
+            <p>Email: <input name="email" type="email"></p>
+            <input type="submit" value="회원가입">
+        </form>
+    </div>
+</body>
+</html>
